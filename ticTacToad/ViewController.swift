98010a3d8c2784503ff33fbe79c8248322722678
enzobotter <enzobot@hotmail.com>
2016-12-09 13:56:51 -0800

    tic tac toe game
    finished the board for the tic tac toe implementation

--- ticTacToad/ViewController.swift
+++ ticTacToad/ViewController.swift
@@ -10,15 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+ 
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let boardHeight = view.frame.size.height/3
+        let boardWidth = view.frame.size.width/3
+        
+        for i in 0..<9{
+            let squarez = UIView(frame: CGRect.zero)
+            squarez.translatesAutoresizingMaskIntoConstraints = false
+            squarez.backgroundColor = .white
+            squarez.layer.borderColor = UIColor.black.cgColor
+            squarez.layer.borderWidth = 3
+            view.addSubview(squarez)
+            
+            NSLayoutConstraint(item: squarez, attribute: .width, relatedBy: .equal, toItem: view, attribute: .width, multiplier: 1/3, constant: 0).isActive = true
+            NSLayoutConstraint(item: squarez, attribute: .height, relatedBy: .equal, toItem: squarez, attribute: .width, multiplier: 1, constant: 0).isActive = true
+            NSLayoutConstraint(item: squarez, attribute: .leading, relatedBy: .equal, toItem: view, attribute: .leading, multiplier: 1, constant: (boardWidth * CGFloat(i % 3))).isActive = true
+       
+            if i > 2 && i <= 5 {
+                NSLayoutConstraint(item: squarez, attribute: .top, relatedBy: .equal, toItem: view, attribute: .top, multiplier: 1, constant: boardWidth).isActive = true
+            }else if i > 5 {
+                NSLayoutConstraint(item: squarez, attribute: .top, relatedBy: .equal, toItem: view, attribute: .top, multiplier: 1, constant: (boardWidth * 2)).isActive = true
+            }
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+
 
 
 }
